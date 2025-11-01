cae13f6d65ae7dee1ea336e8e57ccda6a858271a
Eloi Herman <eloi.herman07@gmail.com>
2025-11-01 15:26:29 -0400

    deuxieme commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
+import sqlite3
 
 from cryptography.fernet import Fernet
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from flask_wtf import CSRFProtect
@@ -18,7 +18,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///app.db")
+db = sqlite3.connect("app.db", check_same_thread=False)
 
 load_dotenv()
 
